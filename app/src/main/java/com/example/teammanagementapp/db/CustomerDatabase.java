31096fc57f7668a9df71a6041668bfdf10c0d93a
cristi2708 <cristi2708@yahoo.com>
2022-11-09 03:01:57 +0200

    TeamManagementApp Database

--- app/src/main/java/com/example/teammanagementapp/db/CustomerDatabase.java
+++ app/src/main/java/com/example/teammanagementapp/db/CustomerDatabase.java
@@ -3,6 +3,9 @@ package com.example.teammanagementapp.db;
 import android.content.Context;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
 
 public class CustomerDatabase {
 }
