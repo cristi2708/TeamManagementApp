f6642514b4c29fa6463f8799f4b97d0045e01a60
cristi2708 <cristi2708@yahoo.com>
2022-11-15 20:03:36 +0200

    TeamManagementApp Database

--- app/src/main/java/com/example/teammanagementapp/db/CustomerDatabase.java
+++ app/src/main/java/com/example/teammanagementapp/db/CustomerDatabase.java
@@ -7,5 +7,6 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+@Database(entities = {CustomerModel.class}, version = 1)
 public class CustomerDatabase {
 }
