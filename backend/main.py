86e63a5e7d618a571c4596a4b15912f4a1e31102
Paula Rusti <rusti.paula78@gmail.com>
2022-12-08 18:08:50 +0200

    added routes for fetching the user profile

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
+from typing import Union
+
 import uvicorn
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Header
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
+import mongo
 import user_api
 
 app = FastAPI(docs_url="/")
@@ -17,6 +20,16 @@ def read_current_user(credentials: HTTPBasicCredentials = Depends(security)):
 @app.get("/hello")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/profile/me")
+async def get_my_profile(x_username: Union[str, None] = Header(default=None)):
+    return await mongo.do_find_user(x_username)
+
+
+@app.get("/profile/{username}")
+async def get_user_profile(username: str):
+    return await mongo.do_find_user(username)
 
 
 app.include_router(user_api.router)
